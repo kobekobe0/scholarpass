9068943ded1345e8477078442df61acc5829060a
Kobe Brian Santos <kobe0santos0@gmail.com>
2024-10-01 14:27:51 +0800

    temp

--- src/pages/StudentHome.jsx
+++ src/pages/StudentHome.jsx
@@ -1,7 +1,10 @@
 import { Outlet } from "react-router-dom";
 import StudentNavbar from "../components/student/StudentNavbar";
+import useAuth from "../helper/useAuth";
 
 export default function StudentHome() {
+    const isLoading = useAuth();
+
     const logout = () => {
         localStorage.removeItem('authToken');
         window.location.href = '/signin';
@@ -12,6 +15,7 @@ export default function StudentHome() {
             <div className="flex flex-col items-center justify-center h-screen">
                 <h1 className="text-4xl text-emerald-800">Student Dashboard</h1>
                 <p className="text-lg">Welcome to your dashboard. Here you can view your grades, attendance, and more.</p>
+                <button onClick={logout}>logout</button>
             </div>
         </div>
     )
